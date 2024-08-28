ff3a81879dd974c05eede4529456ed90235f428b
ShotoMatsubara <shoto0628matsubara@gmail.com>
2024-08-28 22:00:54 +0900

    Qiitaのトレンド記事を取得するサービスの作成

--- config/initializers/qiita_api_client.rb
+++ config/initializers/qiita_api_client.rb
@@ -1,11 +1,12 @@
 require 'faraday'
+require 'faraday_middleware'
 require 'json'
 
 module QiitaApiClient
   class << self
     BASE_URL = 'https://qiita.com/api/v2'
 
-    def get(path, params = {})
+    def get(path, params = {})      
       response = connection.get(path, params)
       handle_response(response)
     end
@@ -19,15 +20,25 @@ module QiitaApiClient
 
     def connection
       @connection ||= Faraday.new(url: BASE_URL) do |faraday|
-        faraday.headers['Authorization'] = "Bearer #{ENV['QIITA_ACCESS_TOKEN']}"
+        faraday.headers['Authorization'] = "Bearer #{ENV['QIITA_TOKEN']}"
         faraday.headers['Content-Type'] = 'application/json'
+        faraday.use FaradayMiddleware::FollowRedirects, limit: 3
         faraday.adapter Faraday.default_adapter
       end
     end
 
     def handle_response(response)
-      if response.success?
+      case response.status
+      when 200..299
         JSON.parse(response.body)
+      when 401
+        raise ApiError.new("Unauthorized: Check your access token")
+      when 403
+        raise ApiError.new("Forbidden: You don't have permission to access this resource")
+      when 404
+        raise ApiError.new("Not Found: The requested resource does not exist")
+      when 429
+        raise ApiError.new("Too Many Requests: You have exceeded the API rate limit")
       else
         raise ApiError.new("API request failed with status #{response.status}: #{response.body}")
       end
@@ -39,7 +50,7 @@ end
 
 # 接続テスト
 begin
-  user = QiitaApiClient.get('/authenticated_user')
+  user = QiitaApiClient.get('authenticated_user')
   Rails.logger.info "Qiita API connection successful. Authenticated as: #{user['id']}"
 rescue QiitaApiClient::ApiError => e
   Rails.logger.error "Failed to initialize Qiita API client: #{e.message}"
